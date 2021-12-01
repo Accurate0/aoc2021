e70c835b3565bf00c09d15f2376b8d63d2394108
Anurag Singh <contact@anurag.sh>
2021-12-02 00:26:14 +0800

    better readable

--- day1/Program.cs
+++ day1/Program.cs
@@ -2,23 +2,29 @@
 
 // task 1
 int val = lines.First();
-Console.WriteLine(@$"Problem 1: {lines.Select(num =>
-{
-    int old = val;
-    val = num;
-    return num > old;
-}).Count(x => x)}");
+
+Console.WriteLine("Problem 1: {0}",
+    lines.Select(num =>
+    {
+        int old = val;
+        val = num;
+
+        return num > old;
+    }).Count(x => x)
+);
 
 // task 2
 int oldWindow = lines.Take(3).Sum();
 int windowIndex = 0;
 
-Console.WriteLine(@$"Problem 2: {Enumerable.Range(9, (int)Math.Ceiling(lines.Count() / 3.0) * 3).Select(_ =>
-{
-    int newWindow = lines.Skip(windowIndex).Take(3).Sum();
-    int old = oldWindow;
-    oldWindow = newWindow;
-    windowIndex++;
-    return newWindow > old;
-}).Count(x => x)
-}");
+Console.WriteLine("Problem 2: {0}",
+    Enumerable.Range(0, (int)Math.Ceiling(lines.Count() / 3.0) * 3).Select(_ =>
+    {
+        int newWindow = lines.Skip(windowIndex).Take(3).Sum();
+        int old = oldWindow;
+        oldWindow = newWindow;
+        windowIndex++;
+
+        return newWindow > old;
+    }).Count(x => x)
+);
