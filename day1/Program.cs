23e2f9ea38e912d5dbfac4fece3b658be6329e91
Anurag Singh <contact@anurag.sh>
2021-12-02 00:18:59 +0800

    Add day1

---
+++ day1/Program.cs
@@ -0,0 +1,29 @@
+﻿var lines = File.ReadAllLines(Environment.GetCommandLineArgs()[1]).Select(line => Int32.Parse(line));
+
+// task 1
+int val = lines.First();
+Console.WriteLine(@$"Problem 1: {lines.Select(num =>
+{
+    int old = val;
+    val = num;
+    return num > old;
+}).Count(x => x)}");
+
+// task 2
+int oldWindow = lines.Take(3).Sum();
+int windowIndex = 0;
+int count = 0;
+
+for (int i = 0; i < (int)Math.Ceiling(lines.Count() / 3.0) * 3; i++)
+{
+    int newWindow = lines.Skip(windowIndex).Take(3).Sum();
+    windowIndex++;
+    if (newWindow > oldWindow)
+    {
+        count++;
+    }
+
+    oldWindow = newWindow;
+}
+
+Console.WriteLine($"Problem 2: {count}");
